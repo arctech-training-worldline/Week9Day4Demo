f454807ce41fef6f505026ab59ca2b3f69d99799
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-02-24 10:35:13 +0530

    Student Listing

---
+++ Week9Day4Demo/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week9Day4Demo.Services;
+
+namespace Week9Day4Demo.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var studentsService = new StudentsService();
+            var students = await studentsService.GetAllStudentsAsync();
+
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Week9Day4Demo/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week9Day4Demo.Models
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Percentage { get; set; }
+    }
+}
