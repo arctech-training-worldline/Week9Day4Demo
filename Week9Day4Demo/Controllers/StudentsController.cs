43c37d899f080903ad1577eca0685ef063d85a25
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-02-24 11:24:48 +0530

    Added Create Controller and View Added presentation document

--- Week9Day4Demo/Controllers/StudentsController.cs
+++ Week9Day4Demo/Controllers/StudentsController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Week9Day4Demo.Models;
 using Week9Day4Demo.Services;
 
 namespace Week9Day4Demo.Controllers
 {
     public class StudentsController : Controller
     {
+        public StudentsController()
+        {
+            
+        }
+
         public async Task<IActionResult> Index()
         {
             var studentsService = new StudentsService();
@@ -16,6 +22,16 @@ namespace Week9Day4Demo.Controllers
 
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student student)
+        {
+            var studentService = new StudentsService();
+            await studentService.InsertAsync(student);
+
             return View();
         }
     }
